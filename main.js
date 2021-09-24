7fd5102a115e436bdd29be723b2ce0b8f6684fcf
Yuri Mallak <yurimallak@gmail.com>
2021-09-24 15:28:37 -0300

    aula 3

--- main.js
+++ main.js
@@ -26,3 +26,34 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+
+/* Testimonials carousel slider swiper */
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mouseWheel: true,
+    keyboard: true
+});
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top', 
+    distance: '30px', 
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .txt, #home .image,
+    #about .img, #about .text, 
+    #services header, #services .card, 
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, 
+    { interval: 100 }
+)
